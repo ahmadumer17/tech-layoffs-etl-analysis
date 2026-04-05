2188bcaeb4548d504e4da4432d997f7515060494
ahmadumer17 <ahmadumer1701@gmail.com>
2026-04-05 13:11:08 +0500

    Create query_3_companies.sql

---
+++ query_3_companies.sql
@@ -0,0 +1,18 @@
+-- ============================================
+-- QUERY 3: TOTAL LAYOFFS BY INDUSTRY
+-- Author: [Your Name]
+-- Dataset: Global Tech Layoffs 2020-2026
+-- ============================================
+
+query_3 = """
+SELECT
+    industry,
+    SUM(total_laid_off)           AS total_layoffs,
+    COUNT(*)                      AS total_events,
+    ROUND(AVG(total_laid_off), 0) AS avg_layoffs_per_event
+FROM layoffs
+WHERE total_laid_off IS NOT NULL
+AND industry != 'Unknown'
+GROUP BY industry
+ORDER BY total_layoffs DESC;
+"""
